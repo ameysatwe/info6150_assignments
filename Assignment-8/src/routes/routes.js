9d3aef6f59564415c6ba280b3df2ee7dfe9a33a0
Amey Satwe <amey10051999@gmail.com>
2024-11-05 12:45:07 -0500

    added multer, file upload and file filter works

--- Assignment-8/src/routes/routes.js
+++ Assignment-8/src/routes/routes.js
@@ -1,5 +1,37 @@
 const User = require("../model/user");
 const { hashPassword, comparePassword } = require("../utils/bcryptutils");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + "." + file.mimetype.split("/")[1]
+    );
+  },
+});
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg"
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      const err = new Error("Only .png, .jpg and .jpeg format allowed!");
+      err.status = 400;
+      return cb(err);
+    }
+  },
+  preservePath: true,
+});
 
 module.exports = async (app) => {
   app.get("/user/getAll", async (req, res) => {
@@ -42,5 +74,9 @@ module.exports = async (app) => {
     }
     await user.deleteOne();
     res.status(200).json(user);
+  });
+
+  app.post("/user/uploadFile", upload.single("file"), async (req, res) => {
+    res.status(200).json(req.file);
   });
 };
