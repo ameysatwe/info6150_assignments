0f13bd3e8455627b8326d83da1081b969867420c
Amey Satwe <amey10051999@gmail.com>
2024-11-07 17:08:12 -0500

    remove unnecessary imports

--- Assignment-8/index.js
+++ Assignment-8/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
@@ -12,24 +11,12 @@ mongoose.connection
     console.log("MongoDB Error: " + error);
   });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(bodyParser.json());
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+app.use(express.json());
 
 const routes = require("./src/routes/routes");
 
 routes(app);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server started on port 3000");
 });
