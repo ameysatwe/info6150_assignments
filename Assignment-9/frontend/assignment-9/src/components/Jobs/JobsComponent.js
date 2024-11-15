3c4e3c5605c5e2ade7283f657239d4a9ac5a2d75
Amey Satwe <amey10051999@gmail.com>
2024-11-14 20:33:51 -0500

    added salary to frontend data

--- Assignment-9/frontend/assignment-9/src/components/Jobs/JobsComponent.js
+++ Assignment-9/frontend/assignment-9/src/components/Jobs/JobsComponent.js
@@ -15,6 +15,7 @@ const jobPosts = [
     description:
       "Join our dynamic team to work on cutting-edge technologies. Develop and maintain sophisticated web applications for our diverse client base.",
     lastUpdated: "Last updated 2 days ago",
+    salary: "$80,000 - $100,000",
     applyLink: "https://example.com/apply/full-stack-developer",
   },
   {
@@ -23,6 +24,7 @@ const jobPosts = [
     description:
       "Elevate our digital marketing strategies to promote our innovative products. Proficiency in SEO, SEM, and social media marketing is highly valued.",
     lastUpdated: "Last updated 1 day ago",
+    salary: "$80,000 - $180,000",
     applyLink: "https://example.com/apply/digital-marketing-specialist",
   },
   {
@@ -31,6 +33,7 @@ const jobPosts = [
     description:
       "Analyze large datasets and build predictive models. Must have expertise in machine learning and data analysis tools.",
     lastUpdated: "Last updated 3 days ago",
+    salary: "$120,000 - $200,000",
     applyLink: "https://example.com/apply/data-scientist",
   },
   {
@@ -39,11 +42,13 @@ const jobPosts = [
     description:
       "Design intuitive user interfaces and engaging user experiences. Work closely with product managers and developers to bring design to life.",
     lastUpdated: "Last updated 4 days ago",
+    salary: "$150,000 - $250,000",
     applyLink: "https://example.com/apply/ux-ui-designer",
   },
   {
     id: 5,
     title: "DevOps Engineer",
+    salary: "$200,000 - $300,000",
     description:
       "Ensure the reliability and scalability of our infrastructure. Work on cloud platforms and CI/CD pipelines.",
     lastUpdated: "Last updated 5 days ago",
@@ -82,6 +87,13 @@ export default function JobCarousel() {
                 sx={{ color: "text.secondary" }}
               >
                 {job.lastUpdated}
+              </Typography>
+              <Typography
+                variant="caption"
+                display="block"
+                sx={{ color: "text.secondary" }}
+              >
+                Salary : {job.salary}
               </Typography>
             </CardContent>
             <CardActions>
