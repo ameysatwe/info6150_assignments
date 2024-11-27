68983d8186a083b2ef26b8ca1ae37443bbbbd61b
Amey Satwe <amey10051999@gmail.com>
2024-11-26 20:57:19 -0500

    cleanup assignment 10

--- Assignment-10/backend/user/getAllImages.js
+++
@@ -1,9 +0,0 @@
-import CompanyImages from "../model/CompanyImages.js";
-
-const getAllImages = async (req, res) => {
-  const images = await CompanyImages.find({});
-
-  return res.status(200).json(images);
-};
-
-export default getAllImages;
--- Assignment-10/backend/user/getAllUsers.js
+++
@@ -1,9 +0,0 @@
-// import User from "../models/user.js";
-import User from "../model/user.js";
-
-const getAllUsers = async (req, res) => {
-  const users = await User.find({}).select("email fullName profilePic");
-  res.status(200).json(users);
-};
-
-export default getAllUsers;
